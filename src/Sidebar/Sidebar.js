642baa5f35d56cc50cd81650778ea42e4972c9cd
aalmeida00 <andre-silva78@hotmail.com>
2020-08-18 20:24:37 -0300

    all the structure has been created, now lets start with sidebar :hear_no_evil:

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Sidebar.css";
+import "./SidebarOption/SidebarOption";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import SidebarOption from "./SidebarOption/SidebarOption";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+
+      <SidebarOption />
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/Sidebar/SidebarOption/SidebarOption.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SidebarOption.css";
+import HomeIcon from "@material-ui/icons/Home";
+
+function SidebarOption() {
+  return (
+    <div className="sidebarOption">
+      <HomeIcon />
+    </div>
+  );
+}
+
+export default SidebarOption;
