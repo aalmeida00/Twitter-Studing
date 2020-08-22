8bf96212fe8f25f76afb3ccd9885d57d86f39d1a
aalmeida00 <andre-silva78@hotmail.com>
2020-08-22 01:23:37 -0300

    :boom: feature: ADD tweet directly to firebase :rocket:

--- src/Feed/TweetBox/TweetBox.js
+++ src/Feed/TweetBox/TweetBox.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@material-ui/core";
-
+import db from "../../firebase";
 function TweetBox() {
+  const [tweetMessage, setTweetMessage = ""] = useState("");
+  const [tweetImage, setTweetImage = ""] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Andre Almeida",
+      username: "@aalmeida0",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://pbs.twimg.com/profile_images/1205325886704226304/m7M4zXtv_400x400.jpg",
+    });
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar src="https://pbs.twimg.com/profile_images/1205325886704226304/m7M4zXtv_400x400.jpg"></Avatar>
-          <input type="text" placeholder="What's happening?" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            type={tweetMessage}
+            placeholder="What's happening?"
+          />
         </div>
         <input
           className="tweetBox__imageInput"
-          type="text"
+          onChange={(e) => setTweetImage(e.target.value)}
+          type={tweetImage}
           placeholder="Optional: Place image URL"
         />
 
-        <Button type="submit" className="tweetBox__tweetButton">
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
           Tweet
         </Button>
       </form>
